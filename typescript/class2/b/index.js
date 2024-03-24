812dc105f15675aa389930cf4d0fe2c33db01f9b
Abuhurairah127 <abuhurairahMuhammadShafique@gmail.com>
2024-03-24 19:40:27 +0500

    first 2 classes code added

---
+++ typescript/class2/b/index.js
@@ -0,0 +1,49 @@
+"use strict";
+console.log("Hello World");
+var a = 10;
+var b = 20;
+var c = 30;
+// var _name = "Abu Hurairah";
+// console.log(_name, "a =", a, "b =", b, "c =", c);
+//Operator
+// Arithmetic Operators
+// +, -, *, /, %
+// console.log(10 + 290);
+// var sum = 10 + 20 + 30 + 40;
+// var diff = sum - 30;
+// var product = 40 * 30;
+// var div = 40 / 30;
+// console.log(sum);
+// console.log(diff);
+// console.log(product);
+// console.log(div);
+// var remaining = 13 % 7;
+// console.log(remaining);
+// Exponent Operator
+// **
+// var cube = 27 ** (1 / 3);
+// console.log(cube);
+// Increment && Decrement operators
+var value = 10;
+// value = value + 1
+var val2 = value++; //10,11
+// val2 = value
+// value = value +1
+var val3 = ++value; //11
+// value = value +1
+// val3 = value
+// where value = 10
+// var sum = value + ++val3 + val2++;
+// 12 +13 +10
+// console.log(sum);
+//34,35,30
+var a = 15;
+var b = 10;
+var result = --a + ++a - --b + b;
+// 14+14 -9+9
+console.log(result);
+var userName = "muhammad";
+// var result = string = `muhammad , ${userName};`
+// b= b-1
+// b = b;
+console.log(`result = ${result}`);
---
+++ typescript/class2/index.ts
@@ -0,0 +1,13 @@
+// console.log("Hello world!");
+// var a: number = 20;
+// var b = "Abu Hurairah";
+// console.log(b);
+
+// Store your name and age in a variable and show as following
+
+// My name is Abu Hurairah and I am 21 years old.
+
+var userName: string = "Abu Hurairah";
+var userAge: number = 21;
+
+console.log("My name is", userName, "I am", userAge, "years old.");
