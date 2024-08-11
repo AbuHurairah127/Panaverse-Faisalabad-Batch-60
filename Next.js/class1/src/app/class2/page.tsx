1e74055bf978cc7b39c4406da2a58942bebf14b4
Abu Hurairah <abuhurairahMuhammadShafique@gmail.com>
2024-08-11 12:52:44 +0500

    class 2 code added

---
+++ Next.js/class1/src/app/class2/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <p className="text-xl text-[#606c38] font-black italic line-through decoration-2  decoration-green-600 text-justify capitalize ">
+        This is my first paragraph
+      </p>
+    </>
+  );
+};
+
+export default page;
